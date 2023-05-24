25510959fbf7f29c2f648ba49f6420c747ece376
Sukhvinder Bhullar <Sukhvinder.Bhullar@EDUCATION.GOV.UK>
2023-05-24 16:25:09 +0100

    Added data based dashboard and project overview page

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -13,13 +13,94 @@ module.exports = {
       "status" : "Not started",
     },
     {
-      "projectTitle" : "Another School",
-      "urnNumber" : "293323",
-      "applicant" : "2anchester City Council",
-      "localAuthority" : "2anchester City COuncil",
-      "deliveryOfficer" : "",
-      "openingDate" : "2 July 2024",
-      "status" : "2ot started",
+      "projectTitle" : "Bolton Spa Primary", 
+      "urnNumber" : "112456", 
+      "applicant" : "Oak Hill Trust", 
+      "localAuthority" : "Bolton", 
+      "deliveryOfficer" : "Alex James", 
+      "openingDate" : "1 September 2023", 
+      "status" : "In progress", 
+    },
+    {
+      "projectTitle": "St. Wilfrid's Primary School",
+      "urnNumber": "102062",
+      "applicant": "Dynamics Trust",
+      "localAuthority": "Warrington",
+      "deliveryOfficer": "Alex James",
+      "openingDate": "3 August 2024",
+      "status": "In progress"
+    },
+    {
+      "projectTitle": "Blessed John Henry Newman Roman Catholic College",
+      "urnNumber": "123920",
+      "applicant": "Kingfisher learning trust",
+      "localAuthority": "Warrington",
+      "deliveryOfficer": "",
+      "openingDate": "1 July 2024",
+      "status": "Not started"
+    },
+    {
+      "projectTitle": "St Luke's Church of England Primary School",
+      "urnNumber": "145616",
+      "applicant": "Lambeth",
+      "localAuthority": "Lambeth",
+      "deliveryOfficer": "Paul Michaels",
+      "openingDate": "7 July 2022",
+      "status": "Opened"
+    },
+    {
+      "projectTitle": "St Thomas of Canterbury Catholic Primary School",
+      "urnNumber": "100357",
+      "applicant": "Hammersmith and Fulham",
+      "localAuthority": "Hammersmith and Fulham",
+      "deliveryOfficer": "",
+      "openingDate": "8 August 2024",
+      "status": "Not started"
+    },
+    {
+      "projectTitle": "Newcastle High School for Girls",
+      "urnNumber": "108538",
+      "applicant": "WISE ACADEMIES",
+      "localAuthority": "Newcastle upon Tyne",
+      "deliveryOfficer": "Jo Lee",
+      "openingDate": "24 August 2023",
+      "status": "In progress"
+    },
+    {
+      "projectTitle": "Lymington Junior School",
+      "urnNumber": "116051",
+      "applicant": "THE KITE ACADEMY TRUST",
+      "localAuthority": "Hampshire",
+      "deliveryOfficer": "",
+      "openingDate": "21 July 2024",
+      "status": "Not started"
+    },
+    {
+      "projectTitle": "St Marys Catholic Academy",
+      "urnNumber": "140114",
+      "applicant": "Blackpool",
+      "localAuthority": "Blackpool",
+      "deliveryOfficer": "Zara Laney",
+      "openingDate": "25 August 2024",
+      "status": "In progress"
+    },
+    {
+      "projectTitle": "Binstead Primary School",
+      "urnNumber": "118176",
+      "applicant": "UNITED LEARNING TRUST",
+      "localAuthority": "Manchester City Council",
+      "deliveryOfficer": "Alex James",
+      "openingDate": "10 August 2022",
+      "status": "Opened"
+    },
+    {
+      "projectTitle": "St James Catholic Primary School",
+      "urnNumber": "141820",
+      "applicant": "ACADEMY TRANSFORMATION TRUST",
+      "localAuthority": "Birmingham",
+      "deliveryOfficer": "Paul Michaels",
+      "openingDate": "14 August 2023",
+      "status": "In progress"
     }
   ]
 }
