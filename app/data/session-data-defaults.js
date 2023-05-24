01fbf5c55cb444a8cd620b74df50e556d19c1e7a
Ming Chan <mingosoft@gmail.com>
2023-05-24 12:00:05 +0100

    Adding data structure

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,26 @@
 module.exports = {
 
   // Insert values here
-  'school-name-1': 'Bolton Spa Primary',
+  'school-name-1': 'Chelsea Spa Primary',
+  'project-list': [
+    {
+      "projectTitle" : "St George's Primary School",
+      "urnNumber" : "193323",
+      "applicant" : "Manchester City Council",
+      "localAuthority" : "Manchester City COuncil",
+      "deliveryOfficer" : "",
+      "openingDate" : "4 July 2024",
+      "status" : "Not started",
+    },
+    {
+      "projectTitle" : "Another School",
+      "urnNumber" : "293323",
+      "applicant" : "2anchester City Council",
+      "localAuthority" : "2anchester City COuncil",
+      "deliveryOfficer" : "",
+      "openingDate" : "2 July 2024",
+      "status" : "2ot started",
+    }
+  ]
 }
+
