eb00bd3dc218136e3217de973c097bb92fa5d235
Ming Chan <mingosoft@gmail.com>
2023-08-23 16:21:13 +0100

    added sprint 2 and changed headings in task list

--- app/routes.js
+++ app/routes.js
@@ -16,3 +16,4 @@ require('./routes/alpha-sprint-3.js')(router);
 require('./routes/alpha-sprint-5.js')(router);
 require('./routes/alpha-sprint-6.js')(router);
 require('./routes/beta-sprint-1.js')(router);
+require('./routes/beta-sprint-2.js')(router);
